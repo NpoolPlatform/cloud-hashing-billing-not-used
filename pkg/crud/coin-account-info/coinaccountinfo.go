ed2fd660731bcfebe68ec957b621b19d8f04c5b2
Zhao KK <kikakkz@hotmail.com>
2021-11-04 14:25:36 +0000

    Add coin account info apis

--- pkg/crud/coin-account-info/coinaccountinfo.go
+++ pkg/crud/coin-account-info/coinaccountinfo.go
@@ -1,1 +1,23 @@
 package coinaccountinfo
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/cloud-hashing-billing/message/npool"
+)
+
+func Create(ctx context.Context, in *npool.CreateCoinAccountRequest) (*npool.CreateCoinAccountResponse, error) {
+	return nil, nil
+}
+
+func Get(ctx context.Context, in *npool.GetCoinAccountRequest) (*npool.GetCoinAccountResponse, error) {
+	return nil, nil
+}
+
+func GetCoinAccountsByAppUser(ctx context.Context, in *npool.GetCoinAccountsByAppUserRequest) (*npool.GetCoinAccountsByAppUserResponse, error) {
+	return nil, nil
+}
+
+func Delete(ctx context.Context, in *npool.DeleteCoinAccountRequest) (*npool.DeleteCoinAccountResponse, error) {
+	return nil, nil
+}
---
+++ pkg/crud/coin-account-info/coinaccountinfo_test.go
@@ -0,0 +1,40 @@
+package coinaccountinfo
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/cloud-hashing-billing/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-billing/pkg/test-init" //nolint
+
+	"github.com/google/uuid"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+func TestCRUD(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	coinAccount := npool.CoinAccountInfo{
+		CoinTypeID: uuid.New().String(),
+	}
+
+	_, err := Create(context.Background(), &npool.CreateCoinAccountRequest{
+		Info: &coinAccount,
+	})
+	assert.Nil(t, err)
+}
