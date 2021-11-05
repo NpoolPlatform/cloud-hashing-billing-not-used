b53ec498fbc0e62ee9d0264b0ace592806ca93ad
Zhao KK <kikakkz@hotmail.com>
2021-11-05 12:40:25 +0000

    Support coin account transaction detail

--- api/coinaccounttransaction_test.go
+++ api/coinaccounttransaction_test.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"strconv"
 	"testing"
@@ -11,7 +13,20 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/NpoolPlatform/cloud-hashing-billing/message/npool"
+	"github.com/NpoolPlatform/cloud-hashing-billing/pkg/test-init" //nolint
+
+	"github.com/NpoolPlatform/cloud-hashing-billing/pkg/crud/coin-account-info"        //nolint
+	"github.com/NpoolPlatform/cloud-hashing-billing/pkg/crud/coin-account-transaction" //nolint
 )
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
 
 func assertCoinAccountTransaction(t *testing.T, actual, expected *npool.CoinAccountTransaction) {
 	assert.Equal(t, actual.UserID, expected.UserID)
@@ -141,6 +156,100 @@ func TestCoinAccountTransactionCRUD(t *testing.T) { //nolint
 		if assert.Nil(t, err) {
 			assert.Equal(t, info.Info.ID, firstCreateInfo.Info.ID)
 			assertCoinAccountTransaction(t, info.Info, &coinAccountTransaction)
+		}
+	}
+}
+
+func TestGetCoinAccountTransactionDetail(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+
+	coinTypeID := uuid.New().String()
+
+	account1 := npool.CoinAccountInfo{
+		CoinTypeID:  coinTypeID,
+		Address:     uuid.New().String(),
+		GeneratedBy: "user",
+		UsedFor:     "withdraw",
+		AppID:       uuid.New().String(),
+		UserID:      uuid.New().String(),
+	}
+	account2 := npool.CoinAccountInfo{
+		CoinTypeID:  coinTypeID,
+		Address:     uuid.New().String(),
+		GeneratedBy: "platform",
+		UsedFor:     "benefit",
+		AppID:       uuid.UUID{}.String(),
+		UserID:      uuid.UUID{}.String(),
+	}
+
+	resp1, err := coinaccountinfo.Create(context.Background(), &npool.CreateCoinAccountRequest{
+		Info: &account1,
+	})
+	assert.Nil(t, err)
+
+	resp2, err := coinaccountinfo.Create(context.Background(), &npool.CreateCoinAccountRequest{
+		Info: &account2,
+	})
+	assert.Nil(t, err)
+
+	transaction := npool.CoinAccountTransaction{
+		UserID:                resp1.Info.UserID,
+		AppID:                 resp1.Info.AppID,
+		FromAddressID:         resp1.Info.ID,
+		ToAddressID:           resp2.Info.ID,
+		CoinTypeID:            coinTypeID,
+		Amount:                0.13,
+		Message:               "test transaction",
+		ChainTransactionID:    "adfjklasjfdlksajf",
+		PlatformTransactionID: uuid.New().String(),
+	}
+	resp3, err := coinaccounttransaction.Create(context.Background(), &npool.CreateCoinAccountTransactionRequest{
+		Info: &transaction,
+	})
+	assert.Nil(t, err)
+
+	cli := resty.New()
+
+	resp, err := cli.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(npool.GetCoinAccountTransactionDetailRequest{
+			ID: resp3.Info.ID,
+		}).
+		Post("http://localhost:34759/v1/get/coin/account/transaction/detail")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 200, resp.StatusCode())
+		info := npool.GetCoinAccountTransactionDetailResponse{}
+		err := json.Unmarshal(resp.Body(), &info)
+		if assert.Nil(t, err) {
+			assert.Equal(t, info.Detail.ID, resp3.Info.ID)
+			assert.Equal(t, info.Detail.UserID, resp3.Info.UserID)
+			assert.Equal(t, info.Detail.AppID, resp3.Info.AppID)
+
+			assert.Equal(t, info.Detail.FromAddress.ID, resp1.Info.ID)
+			assert.Equal(t, info.Detail.FromAddress.CoinTypeID, resp1.Info.CoinTypeID)
+			assert.Equal(t, info.Detail.FromAddress.Address, resp1.Info.Address)
+			assert.Equal(t, info.Detail.FromAddress.GeneratedBy, resp1.Info.GeneratedBy)
+			assert.Equal(t, info.Detail.FromAddress.UsedFor, resp1.Info.UsedFor)
+			assert.Equal(t, info.Detail.FromAddress.Idle, resp1.Info.Idle)
+			assert.Equal(t, info.Detail.FromAddress.AppID, resp1.Info.AppID)
+			assert.Equal(t, info.Detail.FromAddress.UserID, resp1.Info.UserID)
+
+			assert.Equal(t, info.Detail.ToAddress.ID, resp2.Info.ID)
+			assert.Equal(t, info.Detail.ToAddress.CoinTypeID, resp2.Info.CoinTypeID)
+			assert.Equal(t, info.Detail.ToAddress.Address, resp2.Info.Address)
+			assert.Equal(t, info.Detail.ToAddress.GeneratedBy, resp2.Info.GeneratedBy)
+			assert.Equal(t, info.Detail.ToAddress.UsedFor, resp2.Info.UsedFor)
+			assert.Equal(t, info.Detail.ToAddress.Idle, resp2.Info.Idle)
+			assert.Equal(t, info.Detail.ToAddress.AppID, resp2.Info.AppID)
+			assert.Equal(t, info.Detail.ToAddress.UserID, resp2.Info.UserID)
+
+			assert.Equal(t, info.Detail.Amount, resp3.Info.Amount)
+			assert.Equal(t, info.Detail.Message, resp3.Info.Message)
+			assert.Equal(t, info.Detail.ChainTransactionID, resp3.Info.ChainTransactionID)
+			assert.Equal(t, info.Detail.PlatformTransactionID, resp3.Info.PlatformTransactionID)
+			assert.Equal(t, info.Detail.State, resp3.Info.State)
 		}
 	}
 }
