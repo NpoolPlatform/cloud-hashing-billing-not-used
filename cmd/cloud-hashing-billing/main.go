8c5b7b31c80f4cf1b7b20c0549afbdd3e84e8142
Zhao KK <kikakkz@hotmail.com>
2021-11-24 12:40:15 +0000

    Use new app

--- cmd/cloud-hashing-billing/main.go
+++ cmd/cloud-hashing-billing/main.go
@@ -6,6 +6,11 @@ import (
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/app"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	mysqlconst "github.com/NpoolPlatform/go-service-framework/pkg/mysql/const"
+	rabbitmqconst "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/const"
+	redisconst "github.com/NpoolPlatform/go-service-framework/pkg/redis/const"
+
 	cli "github.com/urfave/cli/v2"
 )
 
@@ -18,7 +23,18 @@ func main() {
 
 	description := fmt.Sprintf("my %v service cli\nFor help on any individual command run <%v COMMAND -h>\n",
 		serviceName, serviceName)
-	err := app.Init(serviceName, description, "", "", "./", nil, commands)
+	err := app.Init(
+		serviceName,
+		description,
+		"",
+		"",
+		"./",
+		nil,
+		commands,
+		mysqlconst.MysqlServiceName,
+		rabbitmqconst.RabbitMQServiceName,
+		redisconst.RedisServiceName,
+	)
 	if err != nil {
 		logger.Sugar().Errorf("fail to create %v: %v", serviceName, err)
 		return
