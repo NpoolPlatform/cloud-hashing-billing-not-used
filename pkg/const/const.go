b47595b7a3eb45b1bca24b8c7b93f1088d1eeaf7
dengjie <dj@lifesdk.com>
2021-12-08 22:55:03 +0800

    add constant transaction type

---
+++ pkg/const/const.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	TransactionStateWait       = "wait"
+	TransactionStatePaying     = "paying"
+	TransactionStateSuccessful = "successful"
+	TransactionStateFail       = "fail"
+)
