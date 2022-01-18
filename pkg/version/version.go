368388b5b9ca634923e822a1d69b57a64bcf728e
Zhao KK <kikakkz@hotmail.com>
2022-01-18 10:25:09 +0000

    Update message version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 import (
-	npool "github.com/NpoolPlatform/message/npool/cloud-hashing-billing"
+	npool "github.com/NpoolPlatform/message/npool"
 
 	cv "github.com/NpoolPlatform/go-service-framework/pkg/version"
 
